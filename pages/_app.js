6e2a8e1253d13346a4275c3bf208db76eb842dc7
cecep januardi <cecepjanuardi31@gmail.com>
2021-10-24 23:10:03 +0700

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
+// import '../styles/tailwind.css'
+
 
 function MyApp({ Component, pageProps }) {
+  localStorage.setItem("token","90042c18ba8d8c0c5a2f1385681d99eac5b55a153c7c3eb08f7e29566e5ba545")
   return <Component {...pageProps} />
 }
 
